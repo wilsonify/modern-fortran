ca0a367c8aac5d471aa07eb4dd90eda193385928
wilsonify <tom.a.wilson@gmail.com>
2023-04-11 22:02:05 -0400

    use working cmakelists

--- src/neural-fortran/h5fortran.cmake
+++
@@ -1,7 +0,0 @@
-set(h5fortran_BUILD_TESTING FALSE)
-#build the rest runner
-#include_directories(external/h5fortran/include)
-find_library(HDF5_LIB hdf5)
-message(STATUS "HDF5 library path: ${HDF5_LIB}")
-add_subdirectory(external/h5fortran)
-#link_directories(external/h5fortran/build)
