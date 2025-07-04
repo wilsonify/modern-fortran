d00059e767428a32c3ff183c6e927644183e4248
wilsonify <tom.andrew.wilson@gmail.com>
2025-07-04 15:59:37 -0400

    user_functions.f90

--- u01-introduction-to-fortran/c03-building-programs/src/e02-tabulate.f90
+++ u01-introduction-to-fortran/c03-building-programs/src/e02-tabulate.f90
@@ -1,13 +1,4 @@
-module user_functions
-    implicit none
-contains
 
-real function f( x )
-    real, intent(in) :: x
-    f = x - x**2 + sin(x)
-end function f
-
-end module user_functions
 
 program tabulate
     use user_functions
