1bbe2963d727e4d4c052c464bf2681d79a0e2d0a
wilsonify <tom.andrew.wilson@gmail.com>
2025-07-04 17:25:53 -0400

    mymath.f90

--- u01-introduction-to-fortran/c03-building-programs/src/mymath.f90
+++ u01-introduction-to-fortran/c03-building-programs/src/mymath.f90
@@ -1,9 +1,17 @@
 module mymath
   implicit none
 contains
+
   function square(x) result(res)
     real, intent(in) :: x
     real :: res
     res = x * x
   end function square
+
+  function cube(x) result(res)
+    real, intent(in) :: x
+    real :: res
+    res = x * x * x
+  end function cube
+
 end module mymath
