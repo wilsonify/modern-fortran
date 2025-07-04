c489153261fb9927b02b9f329443e266aa0a58f1
wilsonify <tom.andrew.wilson@gmail.com>
2025-07-04 13:12:01 -0400

    resolve

---
+++ src/neural-fortran/src/nf/nf_conv2d_layer.f90
@@ -0,0 +1,118 @@
+module nf_conv2d_layer
+
+  !! This modules provides a 2-d convolutional `conv2d_layer` type.
+
+  use nf_activation, only: activation_function
+  use nf_base_layer, only: base_layer
+  implicit none
+
+  private
+  public :: conv2d_layer
+
+  type, extends(base_layer) :: conv2d_layer
+
+    integer :: width
+    integer :: height
+    integer :: channels
+    integer :: kernel_size
+    integer :: filters
+
+    real, allocatable :: biases(:) ! size(filters)
+    real, allocatable :: kernel(:,:,:,:) ! filters x channels x window x window
+    real, allocatable :: output(:,:,:) ! filters x output_width * output_height
+    real, allocatable :: z(:,:,:) ! kernel .dot. input + bias
+
+    real, allocatable :: dw(:,:,:,:) ! weight (kernel) gradients
+    real, allocatable :: db(:) ! bias gradients
+    real, allocatable :: gradient(:,:,:)
+
+    class(activation_function), allocatable :: activation
+
+  contains
+
+    procedure :: init
+    procedure :: forward
+    procedure :: backward
+    procedure :: get_num_params
+    procedure :: get_params
+    procedure :: set_params
+    procedure :: update
+
+  end type conv2d_layer
+
+  interface conv2d_layer
+    pure module function conv2d_layer_cons(filters, kernel_size, activation) &
+      result(res)
+      !! `conv2d_layer` constructor function
+      integer, intent(in) :: filters
+      integer, intent(in) :: kernel_size
+      class(activation_function), intent(in) :: activation
+      type(conv2d_layer) :: res
+    end function conv2d_layer_cons
+  end interface conv2d_layer
+
+  interface
+
+    module subroutine init(self, input_shape)
+      !! Initialize the layer data structures.
+      !!
+      !! This is a deferred procedure from the `base_layer` abstract type.
+      class(conv2d_layer), intent(in out) :: self
+        !! A `conv2d_layer` instance
+      integer, intent(in) :: input_shape(:)
+        !! Input layer dimensions
+    end subroutine init
+
+    pure module subroutine forward(self, input)
+      !! Apply a forward pass on the `conv2d` layer.
+      class(conv2d_layer), intent(in out) :: self
+        !! A `conv2d_layer` instance
+      real, intent(in) :: input(:,:,:)
+        !! Input data
+    end subroutine forward
+
+    pure module subroutine backward(self, input, gradient)
+      !! Apply a backward pass on the `conv2d` layer.
+      class(conv2d_layer), intent(in out) :: self
+        !! A `conv2d_layer` instance
+      real, intent(in) :: input(:,:,:)
+        !! Input data (previous layer)
+      real, intent(in) :: gradient(:,:,:)
+        !! Gradient (next layer)
+    end subroutine backward
+
+    pure module function get_num_params(self) result(num_params)
+      !! Get the number of parameters in the layer.
+      class(conv2d_layer), intent(in) :: self
+        !! A `conv2d_layer` instance
+      integer :: num_params
+        !! Number of parameters
+    end function get_num_params
+
+    pure module function get_params(self) result(params)
+      !! Get the parameters of the layer.
+      class(conv2d_layer), intent(in) :: self
+        !! A `conv2d_layer` instance
+      real, allocatable :: params(:)
+        !! Parameters to get
+    end function get_params
+
+    module subroutine set_params(self, params)
+      !! Set the parameters of the layer.
+      class(conv2d_layer), intent(in out) :: self
+        !! A `conv2d_layer` instance
+      real, intent(in) :: params(:)
+        !! Parameters to set
+    end subroutine set_params
+
+    module subroutine update(self, learning_rate)
+      !! Update the weights and biases.
+      class(conv2d_layer), intent(in out) :: self
+        !! Dense layer instance
+      real, intent(in) :: learning_rate
+        !! Learning rate (must be > 0)
+    end subroutine update
+
+  end interface
+
+end module nf_conv2d_layer
---
+++ src/neural-fortran/src/nf/nf_layer_constructors.f90
@@ -0,0 +1,171 @@
+module nf_layer_constructors
+
+  !! This module provides the functions to instantiate specific layers.
+
+  use nf_layer, only: layer
+  use nf_activation, only : activation_function
+
+  implicit none
+
+  private
+  public :: conv2d, dense, flatten, input, maxpool2d, reshape
+
+  interface input
+
+    pure module function input1d(layer_size) result(res)
+      !! 1-d input layer constructor.
+      !!
+      !! This layer is for inputting 1-d data to the network.
+      !! Currently, this layer must be followed by a dense layer.
+      !! An input layer must be the first layer in the network.
+      !!
+      !! This is a specific function that is available
+      !! under a generic name `input`.
+      !!
+      !! Example:
+      !!
+      !! ```
+      !! use nf, only :: input, layer
+      !! type(layer) :: input_layer
+      !! input_layer = input(768)
+      !! ```
+      integer, intent(in) :: layer_size
+        !! Size of the input layer
+      type(layer) :: res
+        !! Resulting layer instance
+    end function input1d
+
+    pure module function input3d(layer_shape) result(res)
+      !! 3-d input layer constructor.
+      !!
+      !! This layer is for inputting 3-d data to the network.
+      !! Currently, this layer must be followed by a conv2d layer.
+      !! An input layer must be the first layer in the network.
+      !!
+      !! This is a specific function that is available
+      !! under a generic name `input`.
+      !!
+      !! Example:
+      !!
+      !! ```
+      !! use nf, only :: input, layer
+      !! type(layer) :: input_layer
+      !! input_layer = input([28, 28, 1])
+      !! ```
+      integer, intent(in) :: layer_shape(3)
+        !! Shape of the input layer
+      type(layer) :: res
+        !! Resulting layer instance
+    end function input3d
+
+  end interface input
+
+  interface
+
+    pure module function dense(layer_size, activation) result(res)
+      !! Dense (fully-connected) layer constructor.
+      !!
+      !! This layer is a building block for dense, fully-connected networks,
+      !! or for an output layer of a convolutional network.
+      !! A dense layer must not be the first layer in the network.
+      !!
+      !! Example:
+      !!
+      !! ```
+      !! use nf, only :: dense, layer, relu
+      !! type(layer) :: dense_layer
+      !! dense_layer = dense(10)
+      !! dense_layer = dense(10, activation=relu())
+      !! ```
+      integer, intent(in) :: layer_size
+        !! The number of neurons in a dense layer
+      class(activation_function), intent(in), optional :: activation
+        !! Activation function instance (default sigmoid)
+      type(layer) :: res
+        !! Resulting layer instance
+    end function dense
+
+    pure module function flatten() result(res)
+      !! Flatten (3-d -> 1-d) layer constructor.
+      !!
+      !! Use this layer to chain layers with 3-d outputs to layers with 1-d
+      !! inputs. For example, to chain a `conv2d` or a `maxpool2d` layer
+      !! with a `dense` layer for a CNN for classification, place a `flatten`
+      !! layer between them.
+      !!
+      !! A flatten layer must not be the first layer in the network.
+      !!
+      !! Example:
+      !!
+      !! ```
+      !! use nf, only :: flatten, layer
+      !! type(layer) :: flatten_layer
+      !! flatten_layer = flatten()
+      !! ```
+      type(layer) :: res
+        !! Resulting layer instance
+    end function flatten
+
+    pure module function conv2d(filters, kernel_size, activation) result(res)
+      !! 2-d convolutional layer constructor.
+      !!
+      !! This layer is for building 2-d convolutional network.
+      !! Although the established convention is to call these layers 2-d,
+      !! the shape of the data is actuall 3-d: image width, image height,
+      !! and the number of channels.
+      !! A conv2d layer must not be the first layer in the network.
+      !!
+      !! Example:
+      !!
+      !! ```
+      !! use nf, only :: conv2d, layer
+      !! type(layer) :: conv2d_layer
+      !! conv2d_layer = dense(filters=32, kernel_size=3)
+      !! conv2d_layer = dense(filters=32, kernel_size=3, activation='relu')
+      !! ```
+      integer, intent(in) :: filters
+        !! Number of filters in the output of the layer
+      integer, intent(in) :: kernel_size
+        !! Width of the convolution window, commonly 3 or 5
+      class(activation_function), intent(in), optional :: activation
+        !! Activation function (default sigmoid)
+      type(layer) :: res
+        !! Resulting layer instance
+    end function conv2d
+
+    pure module function maxpool2d(pool_size, stride) result(res)
+      !! 2-d maxpooling layer constructor.
+      !!
+      !! This layer is for downscaling other layers, typically `conv2d`.
+      !!
+      !! Example:
+      !!
+      !! ```
+      !! use nf, only :: maxpool2d, layer
+      !! type(layer) :: maxpool2d_layer
+      !! maxpool2d_layer = maxpool2d(pool_size=2)
+      !! maxpool2d_layer = maxpool2d(pool_size=2, stride=3)
+      !! ```
+      integer, intent(in) :: pool_size
+        !! Width of the pooling window, commonly 2
+      integer, intent(in), optional :: stride
+        !! Stride of the pooling window, commonly equal to `pool_size`;
+        !! Defaults to `pool_size` if omitted.
+      type(layer) :: res
+        !! Resulting layer instance
+    end function maxpool2d
+
+    pure module function reshape(output_shape) result(res)
+      !! Rank-1 to rank-any reshape layer constructor.
+      !! Currently implemented is only rank-3 for the output of the reshape.
+      !!
+      !! This layer is for connecting 1-d inputs to conv2d or similar layers.
+      integer, intent(in) :: output_shape(:)
+        !! Shape of the output
+      type(layer) :: res
+        !! Resulting layer instance
+    end function reshape
+
+  end interface
+
+end module nf_layer_constructors
