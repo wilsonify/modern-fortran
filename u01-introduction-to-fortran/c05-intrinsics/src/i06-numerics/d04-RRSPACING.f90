b94ff9aaa2178c707160370689b8cd39f1f70e32
wilsonify <tom.andrew.wilson@gmail.com>
2025-07-12 15:48:09 -0400

    i06-numerics

--- u01-introduction-to-fortran/c05-intrinsics/src/i06-numerics/d04-RRSPACING.f90
+++ u01-introduction-to-fortran/c05-intrinsics/src/i06-numerics/d04-RRSPACING.f90
@@ -1,54 +1,19 @@
----
-layout: book
-title: rrspacing
-permalink: /learn/intrinsics/RRSPACING
----
+program demo_rrspacing
+  implicit none
+  real :: x, result
 
-## __Name__
+  ! Choose a sample value
+  x = 10.0
 
-__rrspacing__(3) - \[MODEL\_COMPONENTS\] Reciprocal of the relative spacing
+  ! Compute the reciprocal of the relative spacing near x
+  result = rrspacing(x)
 
-## __Syntax__
+  ! Output the result and verify the relationship
+  print *, "x                = ", x
+  print *, "fraction(x)      = ", fraction(x)
+  print *, "radix(x)         = ", radix(x)
+  print *, "digits(x)        = ", digits(x)
+  print *, "rrspacing(x)     = ", result
+  print *, "expected value   = ", abs(fraction(x)) * real(radix(x))**digits(x)
 
-```fortran
-result = rrspacing(x)
-```
-
-## __Description__
-
-__rrspacing(x)__ returns the reciprocal of the relative spacing of model
-numbers near __x__.
-
-## __Arguments__
-
-- __x__
-  : Shall be of type _real_.
-
-## __Returns__
-
-The return value is of the same type and kind as __x__. The value returned
-is equal to __abs(fraction(x)) \* float(radix(x))\*\*digits(x)__.
-
-## __Standard__
-
-Fortran 95 and later
-
-## __See Also__
-
-[__digits__(3)](DIGITS),
-[__epsilon__(3)](EPSILON),
-[__exponent__(3)](EXPONENT),
-[__fraction__(3)](FRACTION),
-[__huge__(3)](HUGE),
-[__maxexponent__(3)](MAXEXPONENT),
-[__minexponent__(3)](MINEXPONENT),
-[__nearest__(3)](NEAREST),
-[__precision__(3)](PRECISION),
-[__radix__(3)](RADIX),
-[__range__(3)](RANGE),
-[__scale__(3)](SCALE),
-[__set\_exponent__(3)](SET_EXPONENT),
-[__spacing__(3)](SPACING),
-[__tiny__(3)](TINY)
-
-###### fortran-lang intrinsic descriptions
+end program demo_rrspacing
