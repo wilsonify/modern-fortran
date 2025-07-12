78653668ac982990b5a7d8e46164445ecb13bd04
wilsonify <tom.andrew.wilson@gmail.com>
2025-07-12 15:41:48 -0400

    i05-interfaces

--- u01-introduction-to-fortran/c05-intrinsics/src/i05-interfaces/d05-C_LOC.f90
+++ u01-introduction-to-fortran/c05-intrinsics/src/i05-interfaces/d05-C_LOC.f90
@@ -1,62 +1,26 @@
----
-layout: book
-title: c_loc
-permalink: /learn/intrinsics/C_LOC
----
+subroutine association_test(a, b)
+    use iso_c_binding, only : c_associated, c_loc, c_ptr
+    implicit none
+    real, pointer :: a
+    type(c_ptr) :: b
+    if(c_associated(b, c_loc(a))) &
+            stop 'b and a do not point to same target'
+end subroutine association_test
+program demo_c_loc
+    use iso_c_binding, only : c_loc, c_ptr, c_associated
+    implicit none
 
-## __Name__
+    real, target :: x = 3.14
+    type(c_ptr) :: p
 
-__c\_loc__(3) - \[ISO\_C\_BINDING\] Obtain the C address of an object
+    ! Get the C address of the Fortran variable x
+    p = c_loc(x)
 
-## __Syntax__
+    ! Confirm association by comparing the C pointer to x
+    if (c_associated(p, c_loc(x))) then
+        print *, "Success: c_loc(x) returned the address of x."
+    else
+        print *, "Error: c_loc(x) does not match x."
+    end if
 
-```fortran
-result = c_loc(x)
-```
-
-## __Description__
-
-__c\_loc(x)__ determines the C address of the argument.
-
-## __Arguments__
-
-- __x__
-  : Shall have either the _pointer_ or _target_ attribute. It shall not be a
-  coindexed object. It shall either be a variable with interoperable
-  type and kind type parameters, or be a scalar, nonpolymorphic
-  variable with no length type parameters.
-
-## __Returns__
-
-The return value is of type c\_ptr and contains the C address of the
-argument.
-
-## __Examples__
-
-Sample program:
-
-```fortran
-   subroutine association_test(a,b)
-   use iso_c_binding, only: c_associated, c_loc, c_ptr
-   implicit none
-   real, pointer :: a
-   type(c_ptr) :: b
-     if(c_associated(b, c_loc(a))) &
-        stop 'b and a do not point to same target'
-   end subroutine association_test
-```
-
-## __Standard__
-
-Fortran 2003 and later
-
-## __See Also__
-
-[__c\_associated__(3)](C_ASSOCIATED),
-[__c\_funloc__(3)](C_FUNLOC),
-[__c\_f\_pointer__(3)](C_F_POINTER),
-
-[__c\_f\_procpointer__(3)](C_F_PROCPOINTER),
-__iso\_c\_binding__(3)
-
-###### fortran-lang intrinsic descriptions
+end program demo_c_loc
