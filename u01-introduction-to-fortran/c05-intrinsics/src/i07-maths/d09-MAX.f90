4ae7f64c966a25bb1c5a7ade4b8e575d3fbe7aa6
wilsonify <tom.andrew.wilson@gmail.com>
2025-07-13 10:24:07 -0400

    i07-maths

--- u01-introduction-to-fortran/c05-intrinsics/src/i07-maths/d09-MAX.f90
+++ u01-introduction-to-fortran/c05-intrinsics/src/i07-maths/d09-MAX.f90
@@ -1,50 +1,3 @@
----
-layout: book
-title: max
-permalink: /learn/intrinsics/MAX
----
-
-## __Name__
-
-__max__(3) - \[NUMERIC\] Maximum value of an argument list
-
-## __Syntax__
-
-```fortran
-result = max(a1, a2, a3, ...)
-```
-
-## __Description__
-
-Returns the argument with the largest (most positive) value.
-
-## __Arguments__
-
-- __a1__
-  : The type shall be _integer_ or _real_.
-
-- __a2,a3,...__
-  : An expression of the same type and kind as __a1__.
-
-## __Returns__
-
-The return value corresponds to the maximum value among the arguments,
-and has the same type and kind as the first argument.
-
-The function is both elemental and allows for an arbitrary number of
-arguments. This means if some elements are scalar and some are arrays
-that all the arrays must be of the same size, and the returned value
-will be an array that is the result as if multiple calls were made with
-all scalar values with a single element of each array used in each call.
-If called with all arrays the returned array is the same as if multiple
-calls were made with __max(arr1(1),arr2(1), ...)__ to
-__max(arr1(N),arr2(N))__.
-
-## __Examples__
-
-Sample program
-
-```fortran
 program demo_max
 implicit none
 real :: arr1(4)= [10.0,11.0,30.0,-100.0]
@@ -102,25 +55,3 @@ real :: arr2(5)= [20.0,21.0,32.0,-200.0,2200.0]
   write(*,*)'scalars and array:',&
   & max(40.0,11.0,30.0,-100.0,arr2(:4),arr1)
 end program demo_max
-```
-
-Results:
-
-```text
-    scalars:   30.000000
-      20.0000000  21.000000  32.000000 -100.00000
-    scalars and array: 30.000000 30.000000 32.000000 30.000000 2200.0000
-    scalars and array: 40.000000 40.000000 40.000000 40.000000
-```
-
-## __Standard__
-
-FORTRAN 77 and later
-
-## __See Also__
-
-[__maxloc__(3)](MAXLOC),
-[__maxval__(3)](MAXVAL),
-[__min__(3)](MIN)
-
-###### fortran-lang intrinsic descriptions
