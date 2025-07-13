74dc9052953d5059b692c117d048dacac3931eff
wilsonify <tom.andrew.wilson@gmail.com>
2025-07-13 11:56:10 -0400

    i13-types

--- u01-introduction-to-fortran/c05-intrinsics/src/i13-types/d09-KIND.f90
+++ u01-introduction-to-fortran/c05-intrinsics/src/i13-types/d09-KIND.f90
@@ -1,9 +1,9 @@
 program demo_kind
-implicit none
-integer,parameter :: kc = kind(' ')
-integer,parameter :: kl = kind(.true.)
+    implicit none
+    integer, parameter :: kc = kind(' ')
+    integer, parameter :: kl = kind(.true.)
 
-   print *, "The default character kind is ", kc
-   print *, "The default logical kind is ", kl
+    print *, "The default character kind is ", kc
+    print *, "The default logical kind is ", kl
 
 end program demo_kind
