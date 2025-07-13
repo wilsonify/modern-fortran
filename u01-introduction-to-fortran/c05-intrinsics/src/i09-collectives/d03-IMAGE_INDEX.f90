d27725822e7165ea5e9263cbfb7b7eff3a2fc5eb
wilsonify <tom.andrew.wilson@gmail.com>
2025-07-13 12:36:32 -0400

    COARRAY_FLAGS = -fcoarray=single

--- u01-introduction-to-fortran/c05-intrinsics/src/i09-collectives/d03-IMAGE_INDEX.f90
+++ u01-introduction-to-fortran/c05-intrinsics/src/i09-collectives/d03-IMAGE_INDEX.f90
@@ -3,4 +3,4 @@ implicit none
 integer :: array[2,-1:4,8,*]
    ! Writes  28 (or 0 if there are fewer than 28 images)
    write (*,*) image_index(array, [2,0,3,1])
-end demo_image_index
+end program demo_image_index
