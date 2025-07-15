015b3d42f7a603f166033c2c3cc2c416dbc6760f
wilsonify <tom.andrew.wilson@gmail.com>
2025-07-15 08:20:28 -0400

    mod_compare

--- u02-functional-fortran/src/c01-basics/compare.f90
+++ u02-functional-fortran/src/c01-basics/compare.f90
@@ -1,3 +1,12 @@
+module mod_compare
+    use iso_fortran_env, only : r4 => real32, r8 => real64, r16 => real128
+    implicit none
+    private
+    public :: ge_c4, ge_c8, ge_c16
+    public :: lt_c4, lt_c8, lt_c16
+
+contains
+
 
     pure elemental logical function ge_c4(lhs, rhs) result(res)
         !! Private `>=` implementation for 4-byte complex numbers.
@@ -39,3 +48,4 @@
         complex(r16), intent(in) :: lhs, rhs
         res = abs(lhs) < abs(rhs)
     end function lt_c16
+end module mod_compare
--- u02-functional-fortran/src/functional.f90
+++ u02-functional-fortran/src/functional.f90
@@ -1,43 +1,85 @@
 module functional
-  use iso_fortran_env, only: &
-      i1 => int8,   i2 => int16,  i4 => int32,  i8 => int64, &
-      r4 => real32, r8 => real64, r16 => real128
+    use iso_fortran_env, only : &
+        i1 => int8, i2 => int16, i4 => int32, i8 => int64, &
+        r4 => real32, r8 => real64, r16 => real128
 
-  use mod_arange
-  use mod_arrstr
-  use mod_complement
-  use mod_empty
-  use mod_filter
-  use mod_foldl
-  use mod_foldr
-  use mod_foldt
-  use mod_head
-  use mod_init
-  use mod_insert
-  use mod_intersection
-  use mod_iterfold
-  use mod_last
-  use mod_limit
-  use mod_map
-  use mod_reverse
-  use mod_set
-  use mod_sort
-  use mod_split
-  use mod_strarr
-  use mod_subscript
-  use mod_tail
-  use mod_unfold
-  use mod_union
+    use mod_compare
+    use mod_arange
+    use mod_arrstr
+    use mod_complement
+    use mod_empty
+    use mod_filter
+    use mod_foldl
+    use mod_foldr
+    use mod_foldt
+    use mod_head
+    use mod_init
+    use mod_insert
+    use mod_intersection
+    use mod_iterfold
+    use mod_last
+    use mod_limit
+    use mod_map
+    use mod_reverse
+    use mod_set
+    use mod_sort
+    use mod_split
+    use mod_strarr
+    use mod_subscript
+    use mod_tail
+    use mod_unfold
+    use mod_union
 
-  implicit none
-  private
+    implicit none
+    private
 
-  public :: arange, arrstr, complement, empty, filter, foldl, foldr, foldt, head, &
-             init, insert, intersection, iterfold, last, limit, map, reverse, set, &
-             sort, split, strarr, subscript, tail, unfold, union
+    ! Expose functions for user access
+    public :: arange, arrstr, complement, empty, filter, foldl, foldr, foldt, head, &
+              init, insert, intersection, iterfold, last, limit, map, reverse, set, &
+              sort, split, strarr, subscript, tail, unfold, union
 
-  public :: operator(.complement.), operator(.head.), operator(.init.), &
-             operator(.intersection.), operator(.last.), operator(.reverse.), &
-             operator(.set.), operator(.sort.), operator(.tail.), operator(.union.)
+    ! Expose functions used in operator interfaces
+    public :: complement, head, init, intersection, last, reverse, set, sort, tail, union
+
+    ! Interface blocks for custom operators
+    interface operator(.complement.)
+        module procedure complement
+    end interface
+
+    interface operator(.head.)
+        module procedure head
+    end interface
+
+    interface operator(.init.)
+        module procedure init
+    end interface
+
+    interface operator(.intersection.)
+        module procedure intersection
+    end interface
+
+    interface operator(.last.)
+        module procedure last
+    end interface
+
+    interface operator(.reverse.)
+        module procedure reverse
+    end interface
+
+    interface operator(.set.)
+        module procedure set
+    end interface
+
+    interface operator(.sort.)
+        module procedure sort
+    end interface
+
+    interface operator(.tail.)
+        module procedure tail
+    end interface
+
+    interface operator(.union.)
+        module procedure union
+    end interface
 
 end module functional
