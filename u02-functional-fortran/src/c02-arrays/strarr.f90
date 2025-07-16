6feffe384b6c23d1794995214d20b14755c42809
wilsonify <tom.andrew.wilson@gmail.com>
2025-07-16 07:41:28 -0400

    c02-arrays

--- u02-functional-fortran/src/c02-arrays/strarr.f90
+++ u02-functional-fortran/src/c02-arrays/strarr.f90
@@ -1,10 +1,18 @@
-pure function strarr(string) result(array)
-  !! Returns an array of len=1 characters given a string.
-  character(len=*), intent(in) :: string !! Input string
-  character(len=1), dimension(:), allocatable :: array
-  integer :: n
-  allocate(array(len(string)))
-  do concurrent(n = 1:len(string))
-    array(n) = string(n:n)
-  enddo
-end function strarr
+module mod_strarr
+    implicit none
+    public :: strarr
+
+contains
+
+    pure function strarr(string) result(array)
+        !! Returns an array of len=1 characters given a string.
+        character(len=*), intent(in) :: string !! Input string
+        character(len=1), dimension(:), allocatable :: array
+        integer :: n
+        allocate(array(len(string)))
+        do concurrent(n = 1:len(string))
+            array(n) = string(n:n)
+        enddo
+    end function strarr
+
+end module mod_strarr
