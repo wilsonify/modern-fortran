a9e26993d303f47350d6f7dd4cf2124d3990742c
wilsonify <tom.andrew.wilson@gmail.com>
2025-07-24 07:26:16 -0400

    !! Subscripts the array `x` along indices `ind`.

--- u02-functional-fortran/src/c02-arrays/subscript.f90
+++ u02-functional-fortran/src/c02-arrays/subscript.f90
@@ -1,12 +1,11 @@
+!! Subscripts the array `x` along indices `ind`.
 module mod_subscript
     use iso_fortran_env, only: i1 => int8, i2 => int16, i4 => int32, i8 => int64, &
                                r4 => real32, r8 => real64, r16 => real128
 contains
 
     pure function subscript_i1(x, ind) result(subscript)
-        !! Subscripts the array `x` along indices `ind`.
         !! This specific procedure is for 1-byte integers.
-        !! Overloaded by generic procedure `subscript`.
         integer(i1), dimension(:), intent(in) :: x !! Input array
         integer(i1), dimension(:), intent(in) :: ind !! Indices to subscript
         integer(i1), dimension(:), allocatable :: subscript
@@ -21,9 +20,7 @@ contains
 
 
     pure function subscript_i2(x, ind) result(subscript)
-        !! Subscripts the array `x` along indices `ind`.
         !! This specific procedure is for 2-byte integers.
-        !! Overloaded by generic procedure `subscript`.
         integer(i2), dimension(:), intent(in) :: x !! Input array
         integer(i2), dimension(:), intent(in) :: ind !! Indices to subscript
         integer(i2), dimension(:), allocatable :: subscript
@@ -38,9 +35,7 @@ contains
 
 
     pure function subscript_i4(x, ind) result(subscript)
-        !! Subscripts the array `x` along indices `ind`.
         !! This specific procedure is for 4-byte integers.
-        !! Overloaded by generic procedure `subscript`.
         integer(i4), dimension(:), intent(in) :: x !! Input array
         integer(i4), dimension(:), intent(in) :: ind !! Indices to subscript
         integer(i4), dimension(:), allocatable :: subscript
@@ -55,9 +50,7 @@ contains
 
 
     pure function subscript_i8(x, ind) result(subscript)
-        !! Subscripts the array `x` along indices `ind`.
         !! This specific procedure is for 8-byte integers.
-        !! Overloaded by generic procedure `subscript`.
         integer(i8), dimension(:), intent(in) :: x !! Input array
         integer(i8), dimension(:), intent(in) :: ind !! Indices to subscript
         integer(i8), dimension(:), allocatable :: subscript
@@ -72,9 +65,7 @@ contains
 
 
     pure function subscript_r4(x, ind) result(subscript)
-        !! Subscripts the array `x` along indices `ind`.
         !! This specific procedure is for 4-byte reals.
-        !! Overloaded by generic procedure `subscript`.
         real(r4), dimension(:), intent(in) :: x !! Input array
         integer(i4), dimension(:), intent(in) :: ind !! Indices to subscript
         real(r4), dimension(:), allocatable :: subscript
@@ -89,9 +80,7 @@ contains
 
 
     pure function subscript_r8(x, ind) result(subscript)
-        !! Subscripts the array `x` along indices `ind`.
         !! This specific procedure is for 8-byte reals.
-        !! Overloaded by generic procedure `subscript`.
         real(r8), dimension(:), intent(in) :: x !! Input array
         integer(i4), dimension(:), intent(in) :: ind !! Indices to subscript
         real(r8), dimension(:), allocatable :: subscript
@@ -106,9 +95,7 @@ contains
 
 
     pure function subscript_r16(x, ind) result(subscript)
-        !! Subscripts the array `x` along indices `ind`.
         !! This specific procedure is for 16-byte reals.
-        !! Overloaded by generic procedure `subscript`.
         real(r16), dimension(:), intent(in) :: x !! Input array
         integer(i4), dimension(:), intent(in) :: ind !! Indices to subscript
         real(r16), dimension(:), allocatable :: subscript
@@ -123,9 +110,7 @@ contains
 
 
     pure function subscript_c4(x, ind) result(subscript)
-        !! Subscripts the array `x` along indices `ind`.
-        !! This specific procedure is for 4-byte complex reals.
-        !! Overloaded by generic procedure `subscript`.
+        !! This specific procedure is for 4-byte complex numbers.
         complex(r4), dimension(:), intent(in) :: x !! Input array
         integer(i4), dimension(:), intent(in) :: ind !! Indices to subscript
         complex(r4), dimension(:), allocatable :: subscript
@@ -140,9 +125,7 @@ contains
 
 
     pure function subscript_c8(x, ind) result(subscript)
-        !! Subscripts the array `x` along indices `ind`.
-        !! This specific procedure is for 8-byte complex reals.
-        !! Overloaded by generic procedure `subscript`.
+        !! This specific procedure is for 8-byte complex numbers.
         complex(r8), dimension(:), intent(in) :: x !! Input array
         integer(i4), dimension(:), intent(in) :: ind !! Indices to subscript
         complex(r8), dimension(:), allocatable :: subscript
@@ -157,9 +140,7 @@ contains
 
 
     pure function subscript_c16(x, ind) result(subscript)
-        !! Subscripts the array `x` along indices `ind`.
-        !! This specific procedure is for 16-byte complex reals.
-        !! Overloaded by generic procedure `subscript`.
+        !! This specific procedure is for 16-byte complex numbers.
         complex(r16), dimension(:), intent(in) :: x !! Input array
         integer(i4), dimension(:), intent(in) :: ind !! Indices to subscript
         complex(r16), dimension(:), allocatable :: subscript
