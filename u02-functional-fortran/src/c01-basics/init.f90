39759f18260ea7cb097c62538029216488dc1088
wilsonify <tom.andrew.wilson@gmail.com>
2025-07-24 06:41:51 -0400

    .init operator

--- u02-functional-fortran/src/c01-basics/init.f90
+++ u02-functional-fortran/src/c01-basics/init.f90
@@ -1,125 +1,230 @@
+!! Returns all elements of `x` but the last.
 module mod_init
-    use iso_fortran_env, only : i1 => int8, i2 => int16, i4 => int32, i8 => int64, &
-            r4 => real32, r8 => real64, r16 => real128
+    use iso_fortran_env, only : &
+        i1 => int8, i2 => int16, i4 => int32, i8 => int64, &
+        r4 => real32, r8 => real64, r16 => real128
 
     implicit none
     private
-    interface init
-        module procedure init_i1, init_i2, init_i4, init_i8, &
-                init_r4, init_r8, init_r16, init_c4, init_c8, init_c16, init_char
-    end interface init
+    public :: init
+    public :: operator(.init.)
 
+    interface init
+        module procedure init_i1, init_i2, init_i4, init_i8
+        module procedure init_r4, init_r8, init_r16
+        module procedure init_c4, init_c8, init_c16
+        module procedure init_char
+    end interface
+
+    interface operator(.init.)
+        module procedure op_init_i1, op_init_i2, op_init_i4, op_init_i8
+        module procedure op_init_r4, op_init_r8, op_init_r16
+        module procedure op_init_c4, op_init_c8, op_init_c16
+        module procedure op_init_char
+    end interface
 
 contains
 
+    !===== INTEGER =====
     pure function init_i1(x) result(init)
-        !! Returns all elements of `x` but the last.
         !! This specific procedure is for 1-byte integers.
-        !! Overloaded by generic procedure `init`.
-        integer(i1), dimension(:), intent(in) :: x !! Input array
-        integer(i1), dimension(size(x) - 1) :: init
-        init = x(:size(x) - 1)
+        integer(i1), dimension(:), intent(in) :: x
+        integer(i1), dimension(:), allocatable :: init
+        if (size(x) > 1) then
+            allocate(init(size(x) - 1))
+            init = x(:size(x) - 1)
+        else
+            allocate(init(0))
+        end if
     end function init_i1
 
-
     pure function init_i2(x) result(init)
-        !! Returns all elements of `x` but the last.
         !! This specific procedure is for 2-byte integers.
-        !! Overloaded by generic procedure `init`.
-        integer(i2), dimension(:), intent(in) :: x !! Input array
-        integer(i2), dimension(size(x) - 1) :: init
-        init = x(:size(x) - 1)
+        integer(i2), dimension(:), intent(in) :: x
+        integer(i2), dimension(:), allocatable :: init
+        if (size(x) > 1) then
+            allocate(init(size(x) - 1))
+            init = x(:size(x) - 1)
+        else
+            allocate(init(0))
+        end if
     end function init_i2
 
-
     pure function init_i4(x) result(init)
-        !! Returns all elements of `x` but the last.
         !! This specific procedure is for 4-byte integers.
-        !! Overloaded by generic procedure `init`.
-        integer(i4), dimension(:), intent(in) :: x !! Input array
-        integer(i4), dimension(size(x) - 1) :: init
-        init = x(:size(x) - 1)
+        integer(i4), dimension(:), intent(in) :: x
+        integer(i4), dimension(:), allocatable :: init
+        if (size(x) > 1) then
+            allocate(init(size(x) - 1))
+            init = x(:size(x) - 1)
+        else
+            allocate(init(0))
+        end if
     end function init_i4
 
-
     pure function init_i8(x) result(init)
-        !! Returns all elements of `x` but the last.
         !! This specific procedure is for 8-byte integers.
-        !! Overloaded by generic procedure `init`.
-        integer(i8), dimension(:), intent(in) :: x !! Input array
-        integer(i8), dimension(size(x) - 1) :: init
-        init = x(:size(x) - 1)
+        integer(i8), dimension(:), intent(in) :: x
+        integer(i8), dimension(:), allocatable :: init
+        if (size(x) > 1) then
+            allocate(init(size(x) - 1))
+            init = x(:size(x) - 1)
+        else
+            allocate(init(0))
+        end if
     end function init_i8
 
-
+    !===== REAL =====
     pure function init_r4(x) result(init)
-        !! Returns all elements of `x` but the last.
         !! This specific procedure is for 4-byte reals.
-        !! Overloaded by generic procedure `init`.
-        real(r4), dimension(:), intent(in) :: x !! Input array
-        real(r4), dimension(size(x) - 1) :: init
-        init = x(:size(x) - 1)
+        real(r4), dimension(:), intent(in) :: x
+        real(r4), dimension(:), allocatable :: init
+        if (size(x) > 1) then
+            allocate(init(size(x) - 1))
+            init = x(:size(x) - 1)
+        else
+            allocate(init(0))
+        end if
     end function init_r4
 
-
     pure function init_r8(x) result(init)
-        !! Returns all elements of `x` but the last.
         !! This specific procedure is for 8-byte reals.
-        !! Overloaded by generic procedure `init`.
-        real(r8), dimension(:), intent(in) :: x !! Input array
-        real(r8), dimension(size(x) - 1) :: init
-        init = x(:size(x) - 1)
+        real(r8), dimension(:), intent(in) :: x
+        real(r8), dimension(:), allocatable :: init
+        if (size(x) > 1) then
+            allocate(init(size(x) - 1))
+            init = x(:size(x) - 1)
+        else
+            allocate(init(0))
+        end if
     end function init_r8
 
-
     pure function init_r16(x) result(init)
-        !! Returns all elements of `x` but the last.
-        !! This specific procedure is for 8-byte reals.
-        !! Overloaded by generic procedure `init`.
-        real(r16), dimension(:), intent(in) :: x !! Input array
-        real(r16), dimension(size(x) - 1) :: init
-        init = x(:size(x) - 1)
+        !! This specific procedure is for 16-byte reals.
+        real(r16), dimension(:), intent(in) :: x
+        real(r16), dimension(:), allocatable :: init
+        if (size(x) > 1) then
+            allocate(init(size(x) - 1))
+            init = x(:size(x) - 1)
+        else
+            allocate(init(0))
+        end if
     end function init_r16
 
-
+    !===== COMPLEX =====
     pure function init_c4(x) result(init)
-        !! Returns all elements of `x` but the last.
-        !! This specific procedure is for 4-byte complex reals.
-        !! Overloaded by generic procedure `init`.
-        complex(r4), dimension(:), intent(in) :: x !! Input array
-        complex(r4), dimension(size(x) - 1) :: init
-        init = x(:size(x) - 1)
+        !! This specific procedure is for 4-byte complex.
+        complex(r4), dimension(:), intent(in) :: x
+        complex(r4), dimension(:), allocatable :: init
+        if (size(x) > 1) then
+            allocate(init(size(x) - 1))
+            init = x(:size(x) - 1)
+        else
+            allocate(init(0))
+        end if
     end function init_c4
 
-
     pure function init_c8(x) result(init)
-        !! Returns all elements of `x` but the last.
-        !! This specific procedure is for 8-byte complex reals.
-        !! Overloaded by generic procedure `init`.
-        complex(r8), dimension(:), intent(in) :: x !! Input array
-        complex(r8), dimension(size(x) - 1) :: init
-        init = x(:size(x) - 1)
+        !! This specific procedure is for 8-byte complex.
+        complex(r8), dimension(:), intent(in) :: x
+        complex(r8), dimension(:), allocatable :: init
+        if (size(x) > 1) then
+            allocate(init(size(x) - 1))
+            init = x(:size(x) - 1)
+        else
+            allocate(init(0))
+        end if
     end function init_c8
 
-
     pure function init_c16(x) result(init)
-        !! Returns all elements of `x` but the last.
-        !! This specific procedure is for 8-byte complex reals.
-        !! Overloaded by generic procedure `init`.
-        complex(r16), dimension(:), intent(in) :: x !! Input array
-        complex(r16), dimension(size(x) - 1) :: init
-        init = x(:size(x) - 1)
+        !! This specific procedure is for 16-byte complex.
+        complex(r16), dimension(:), intent(in) :: x
+        complex(r16), dimension(:), allocatable :: init
+        if (size(x) > 1) then
+            allocate(init(size(x) - 1))
+            init = x(:size(x) - 1)
+        else
+            allocate(init(0))
+        end if
     end function init_c16
 
-
+    !===== CHARACTER =====
     pure function init_char(x) result(init)
-        !! Returns all elements of `x` but the last.
         !! This specific procedure is for character string.
-        !! Overloaded by generic procedure `init`.
-        character(len = *), intent(in) :: x !! Input array
-        character(len = len(x) - 1) :: init
-        init = x(:len(x) - 1)
+        character(len=*), intent(in) :: x
+        character(len=len(x) - 1) :: init
+        if (len(x) > 1) then
+            init = x(:len(x) - 1)
+        else
+            init = ''
+        end if
     end function init_char
 
+    !===== OPERATOR FOR EACH TYPE =====
+    pure function op_init_i1(x)
+        integer(i1), dimension(:), intent(in) :: x
+        integer(i1), allocatable, dimension(:) :: op_init_i1
+        op_init_i1 = init(x)
+    end function op_init_i1
+
+    pure function op_init_i2(x)
+        integer(i2), dimension(:), intent(in) :: x
+        integer(i2), allocatable, dimension(:) :: op_init_i2
+        op_init_i2 = init(x)
+    end function op_init_i2
+
+    pure function op_init_i4(x)
+        integer(i4), dimension(:), intent(in) :: x
+        integer(i4), allocatable, dimension(:) :: op_init_i4
+        op_init_i4 = init(x)
+    end function op_init_i4
+
+    pure function op_init_i8(x)
+        integer(i8), dimension(:), intent(in) :: x
+        integer(i8), allocatable, dimension(:) :: op_init_i8
+        op_init_i8 = init(x)
+    end function op_init_i8
+
+    pure function op_init_r4(x)
+        real(r4), dimension(:), intent(in) :: x
+        real(r4), allocatable, dimension(:) :: op_init_r4
+        op_init_r4 = init(x)
+    end function op_init_r4
+
+    pure function op_init_r8(x)
+        real(r8), dimension(:), intent(in) :: x
+        real(r8), allocatable, dimension(:) :: op_init_r8
+        op_init_r8 = init(x)
+    end function op_init_r8
+
+    pure function op_init_r16(x)
+        real(r16), dimension(:), intent(in) :: x
+        real(r16), allocatable, dimension(:) :: op_init_r16
+        op_init_r16 = init(x)
+    end function op_init_r16
+
+    pure function op_init_c4(x)
+        complex(r4), dimension(:), intent(in) :: x
+        complex(r4), allocatable, dimension(:) :: op_init_c4
+        op_init_c4 = init(x)
+    end function op_init_c4
+
+    pure function op_init_c8(x)
+        complex(r8), dimension(:), intent(in) :: x
+        complex(r8), allocatable, dimension(:) :: op_init_c8
+        op_init_c8 = init(x)
+    end function op_init_c8
+
+    pure function op_init_c16(x)
+        complex(r16), dimension(:), intent(in) :: x
+        complex(r16), allocatable, dimension(:) :: op_init_c16
+        op_init_c16 = init(x)
+    end function op_init_c16
+
+    pure function op_init_char(x)
+        character(len=*), intent(in) :: x
+        character(len=len(x) - 1) :: op_init_char
+        op_init_char = init(x)
+    end function op_init_char
 
 end module mod_init
