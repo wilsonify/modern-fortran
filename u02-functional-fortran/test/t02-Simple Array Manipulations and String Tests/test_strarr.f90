f001dc232d64ecf7e640d42a759564c5f603dc73
wilsonify <tom.andrew.wilson@gmail.com>
2025-07-24 07:01:00 -0400

    test_string_to_chars

--- u02-functional-fortran/test/t02-Simple Array Manipulations and String Tests/test_strarr.f90
+++ u02-functional-fortran/test/t02-Simple Array Manipulations and String Tests/test_strarr.f90
@@ -1,31 +1,30 @@
-program test_strarr
-use testing, only:assert, initialize_tests, report_tests
-use functional, only:arrstr, strarr, empty
+program test_string_to_chars
+    use testing, only : assert, initialize_tests, report_tests
+    use functional, only : chars_to_string, string_to_chars, empty
 
-implicit none
+    implicit none
 
-logical, dimension(:), allocatable :: tests
-logical :: test_failed
-integer :: n, ntests
+    logical, dimension(:), allocatable :: tests
+    logical :: test_failed
+    integer :: n, ntests
 
-n = 1
-ntests = 3
-call initialize_tests(tests, ntests)
+    n = 1
+    ntests = 3
+    call initialize_tests(tests, ntests)
 
-tests(n) = assert(all(strarr('hello') == ['h', 'e', 'l', 'l', 'o']), &
-                  'strarr converts to array')
-n = n + 1
+    tests(n) = assert(all(string_to_chars('hello') == ['h', 'e', 'l', 'l', 'o']), &
+            'string_to_chars converts string to character array')
+    n = n + 1
 
-tests(n) = assert(all(strarr('') == empty(' ')), &
-                  'strarr converts empty string to []')
-n = n + 1
+    tests(n) = assert(all(string_to_chars('') == empty(' ')), &
+            'string_to_chars converts empty string to empty array')
+    n = n + 1
 
-tests(n) = assert(arrstr(strarr('hello')) == 'hello', &
-                  'arrstr(strarr(string)) == string')
-n = n + 1
+    tests(n) = assert(chars_to_string(string_to_chars('hello')) == 'hello', &
+            'chars_to_string(string_to_chars(string)) == string')
+    n = n + 1
 
-test_failed = .false.
-call report_tests(tests, test_failed)
-if(test_failed)stop 1
-
-end program test_strarr
+    test_failed = .false.
+    call report_tests(tests, test_failed)
+    if (test_failed) stop 1
+end program test_string_to_chars
