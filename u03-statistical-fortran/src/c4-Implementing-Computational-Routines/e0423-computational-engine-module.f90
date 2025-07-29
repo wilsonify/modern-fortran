0cb35095d749aaf762a4408174a165664508c804
wilsonify <tom.andrew.wilson@gmail.com>
2025-07-29 08:16:56 -0400

    e0423-computational-engine-module.f90

---
+++ u03-statistical-fortran/src/c4-Implementing-Computational-Routines/e0423-computational-engine-module.f90
@@ -0,0 +1,17 @@
+!##################################################################
+module em_exponential_engine
+    use error_handler
+    use program_constants
+    implicit none
+    private ! by default
+    public :: run_em_exponential
+    ! parameters private to this module
+    character(len = *), parameter :: modname = "em_exponential_engine"
+    !###############################################################
+contains
+    !###############################################################
+    ! all public and private procedures will be placed here
+    !###############################################################
+end module em_exponential_engine
+!##################################################################
+
