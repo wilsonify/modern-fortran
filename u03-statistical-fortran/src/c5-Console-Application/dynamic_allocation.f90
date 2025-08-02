560b797d847560c62da2ed94cc87305f0d13465f
wilsonify <tom.andrew.wilson@gmail.com>
2025-08-02 09:45:00 -0400

    dynamic_allocation.f90

--- u03-statistical-fortran/src/c5-Console-Application/dynamic_allocation.f90
+++ u03-statistical-fortran/src/c5-Console-Application/dynamic_allocation.f90
@@ -4,6 +4,16 @@ module dynamic_allocation_mod
     integer, parameter :: RETURN_SUCCESS = 0
     integer, parameter :: RETURN_FAIL = -1
 
+    interface dyn_alloc
+        module procedure dyn_alloc_int
+        module procedure dyn_alloc_char
+    end interface
+
+    interface dyn_dealloc
+        module procedure dyn_dealloc_int
+        module procedure dyn_dealloc_char
+    end interface
+
 contains
 
     !----------------------------------------------------------------
@@ -15,51 +25,42 @@ contains
 
         if (associated(ptr)) then
             deallocate(ptr)
+            nullify(ptr)
         end if
 
-        allocate(ptr(n), stat=status)
-        if (status /= 0) then
-            status = RETURN_FAIL
-        end if
+        allocate(ptr(n), stat = status)
+        if (status /= 0) status = RETURN_FAIL
     end function dyn_alloc_int
 
     !----------------------------------------------------------------
-    ! Allocate character(len=var_len) pointer arrays
     integer function dyn_alloc_char(ptr, n, var_len) result(status)
         integer, intent(in) :: n, var_len
-        character(len=:), pointer :: ptr(:)
+        character(len = :), allocatable :: ptr(:)
+        character(len = var_len) :: mold_var
         integer :: i, stat
 
         status = RETURN_SUCCESS
-        if (associated(ptr)) then
-            do i = 1, size(ptr)
-                if (associated(ptr(i))) nullify(ptr(i))
-            end do
+
+        if (allocated(ptr)) then
             deallocate(ptr)
         end if
 
-        allocate(ptr(n), stat=stat)
+        mold_var = ''  ! length var_len filled with spaces
+
+        allocate(ptr(n), mold = mold_var, stat = stat)
         if (stat /= 0) then
             status = RETURN_FAIL
             return
         end if
 
-        ! Set length of each character element
         do i = 1, n
-            allocate(character(len=var_len) :: ptr(i), stat=stat)
+            allocate(character(len = var_len) :: ptr(i), stat = stat)
             if (stat /= 0) then
                 status = RETURN_FAIL
                 return
             end if
         end do
     end function dyn_alloc_char
-
-    !----------------------------------------------------------------
-    ! Generic interface for dyn_alloc
-    interface dyn_alloc
-        module procedure dyn_alloc_int
-        module procedure dyn_alloc_char
-    end interface
 
     !----------------------------------------------------------------
     ! Deallocate integer pointer arrays safely
@@ -72,25 +73,12 @@ contains
     end subroutine dyn_dealloc_int
 
     !----------------------------------------------------------------
-    ! Deallocate character pointer arrays safely
+    ! Deallocate allocatable deferred-length character arrays safely
     subroutine dyn_dealloc_char(ptr)
-        character(len=:), pointer :: ptr(:)
-        integer :: i
-
-        if (associated(ptr)) then
-            do i = 1, size(ptr)
-                if (associated(ptr(i))) nullify(ptr(i))
-            end do
+        character(len = :), allocatable :: ptr(:)
+        if (allocated(ptr)) then
             deallocate(ptr)
-            nullify(ptr)
         end if
     end subroutine dyn_dealloc_char
-
-    !----------------------------------------------------------------
-    ! Generic interface for dyn_dealloc
-    interface dyn_dealloc
-        module procedure dyn_dealloc_int
-        module procedure dyn_dealloc_char
-    end interface
 
 end module dynamic_allocation_mod
